94a7c14de4226c093fea0626c543c1747a594798
ZhangChunXian <1836891291@qq.com>
2025-05-13 19:29:49 +0800

    05_更新布局和页面内容，替换字体为Karla，并在页面中添加欢迎标题

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <h1 className='text-2xl font-karla'>Welcome to Loom Clone</h1>
+    </div>
   )
 }
 
