cfb5a30af147a6d361176398bb8bfdf3b91b89c7
ZhangChunXian <1836891291@qq.com>
2025-05-13 21:37:57 +0800

    07_重构导航栏，添加Logo和用户头像功能

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const user = {};
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="navbar">
+      <nav>
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={32}
+            height={32}
+          ></Image>
+          <h1>SnapCast</h1>
+        </Link>
+
+        {user && (
+          <figure>
+            <button>
+              <Image
+                src="/assets/images/dummy.jpg"
+                alt="User"
+                width={36}
+                height={36}
+                className="rounded-full ascpect-square"
+              ></Image>
+            </button>
+            <button className="cursor-pointer">
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}></Image>
+            </button>
+          </figure>
+        )
+
+        }
+      </nav>
+    </header>
   )
 }
 
